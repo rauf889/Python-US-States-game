378d9da58f2e558b46cb8817373bf15de1945363
Rauf <92133639+rauf889@users.noreply.github.com>
2024-07-09 14:37:08 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's  name?").title()
+    print(answer_state)
+
+    # if answer states is one of the states in all the states of the 50states.csv:
+        # if they got it right:
+            # create a turtle to write the name of the state at the state's x & y scoreboard
+
+    if answer_state == "Exit":
+        # save the missing states to the csv
+        # states_to_learn.csv
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        # also can use this one:
+        # t.write(state_data.state.item())
