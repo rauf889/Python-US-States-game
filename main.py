1ea9c8978b2a1de93d7c9a7370f107bb9ced7d13
Rauf <92133639+rauf889@users.noreply.github.com>
2024-07-22 08:17:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -19,19 +19,12 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's  name?").title()
-    print(answer_state)
-
-    # if answer states is one of the states in all the states of the 50states.csv:
-        # if they got it right:
-            # create a turtle to write the name of the state at the state's x & y scoreboard
-
     if answer_state == "Exit":
-        # save the missing states to the csv
-        # states_to_learn.csv
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+
+        # *** save the missing states to the csv ***
+        # *** states_to_learn.csv ***
+
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
